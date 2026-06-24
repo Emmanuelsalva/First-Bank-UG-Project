4b7a4e3c4240027d2c26d4967b66a0052262fc76
Emmanuelsalva <salvatoreemmanuel05@gmail.com>
2026-06-24 14:29:32 +0300

    Add files via upload

---
+++ FirstBankPoject1/src/main/java/vu/firstbankpoject1/FirstBankPoject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vu.firstbankpoject1;
+
+/**
+ *
+ * @author DELL
+ */
+public class FirstBankPoject1 {
+
+    public static void main(String[] args) {
+        Login Info = new Login ();
+        Info.setVisible(true);
+    }
+}
